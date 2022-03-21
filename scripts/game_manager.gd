72d63390c1b42c3c84887f6e6348a77fd438bfd2
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-03-21 08:13:23 +0700

    Implement Game Manager
    Also added a main menu scene

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,2 +1,30 @@
 extends Node
 
+
+signal initialized()
+
+var game_path := ""
+
+
+func _ready() -> void:
+	yield(choose_game_path(), "completed")
+	emit_signal("initialized")
+	print("Load main menu")
+	var err := get_tree().change_scene("res://scenes/mainmenu/mainmenu.tscn")
+	assert(err == OK)
+
+
+# Promp the user for the game path
+func choose_game_path() -> void:
+	print("Asking user for game path")
+	var filediag := FileDialog.new()
+	filediag.access = FileDialog.ACCESS_FILESYSTEM
+	filediag.mode = FileDialog.MODE_OPEN_DIR
+	filediag.window_title = "Choose the game directory"
+
+	add_child(filediag)
+	filediag.popup_centered_minsize(Vector2(600, 500))
+	game_path = yield(filediag, "dir_selected")
+	filediag.queue_free()
+
+	print("Game path: " + game_path)
