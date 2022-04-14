9eaecb62b358b9f30efc4a36b4f87b26f25e6660
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-04-14 15:59:19 +0700

    Basic metadata stuff

--- scripts/renderware/kaitai_stream.gd
+++ scripts/renderware/kaitai_stream.gd
@@ -7,3 +7,7 @@ var _io: File
 
 func _init(io: File) -> void:
 	_io = io
+
+
+func read_u4le() -> int:
+	return _io.get_32()
--- scripts/renderware/renderware_binary_stream.gd
+++ scripts/renderware/renderware_binary_stream.gd
@@ -2,10 +2,21 @@ class_name RenderWareBinaryStream
 extends KaitaiStruct
 
 
+var code: int
+var size: int
+var library_id_stamp: int
+
 var _parent: KaitaiStruct
 var _root: RenderWareBinaryStream
 
 
-func _init(io: KaitaiStream, parent: KaitaiStruct = null, root: RenderWareBinaryStream = null).(io):
+func _init(io: KaitaiStream, parent: KaitaiStruct = null, root: RenderWareBinaryStream = null).(io) -> void:
 	_parent = parent
 	_root = root if root != null else self
+	_read()
+
+
+func _read() -> void:
+	code = _io.read_u4le()
+	size = _io.read_u4le()
+	library_id_stamp = _io.read_u4le()
