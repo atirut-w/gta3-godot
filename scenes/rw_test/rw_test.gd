50523c6edd50c46ac419238e7aaef4a3db34960e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-04-14 15:27:19 +0700

    Update test codes

--- scenes/rw_test/rw_test.gd
+++ scenes/rw_test/rw_test.gd
@@ -2,5 +2,10 @@ extends Control
 
 
 func _ready() -> void:
-	var rwbinstream := RenderWareBinaryStream.new()
+	var file := File.new()
+	var err := file.open(GameManager.game_path + "/models/menu.txd", File.READ)
+	if err != OK:
+		return
+	
+	var rwbinstream := RenderWareBinaryStream.new(KaitaiStream.new(file))
 	pass
