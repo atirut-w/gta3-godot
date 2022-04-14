503465f19055ff14a3076f498ba8712cf07caa91
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-04-14 15:26:25 +0700

    Update `Kaitai{Struct, Stream}`

--- scripts/renderware/kaitai_stream.gd
+++ scripts/renderware/kaitai_stream.gd
@@ -1,2 +1,9 @@
 class_name KaitaiStream
 extends Reference
+
+
+var _io: File
+
+
+func _init(io: File) -> void:
+	_io = io
--- scripts/renderware/kaitai_struct.gd
+++ scripts/renderware/kaitai_struct.gd
@@ -1,2 +1,9 @@
 class_name KaitaiStruct
 extends Reference
+
+
+var _io: KaitaiStream
+
+
+func _init(io: KaitaiStream) -> void:
+	_io = io
