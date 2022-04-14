9ba57f00393cb0be8d243369e02d16350c1c877a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-04-14 15:27:07 +0700

    Binary Stream constructor

--- scripts/renderware/renderware_binary_stream.gd
+++ scripts/renderware/renderware_binary_stream.gd
@@ -1,2 +1,11 @@
 class_name RenderWareBinaryStream
 extends KaitaiStruct
+
+
+var _parent: KaitaiStruct
+var _root: RenderWareBinaryStream
+
+
+func _init(io: KaitaiStream, parent: KaitaiStruct = null, root: RenderWareBinaryStream = null).(io):
+	_parent = parent
+	_root = root if root != null else self
