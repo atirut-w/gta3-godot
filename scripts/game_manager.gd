a4b6faed733530f9d83cfdd00c887b09f5cd5420
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-05-24 00:05:55 +0700

    Refactor game manager

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,28 +3,29 @@ extends Node
 
 signal initialized()
 
-var game_path := ""
+var game_path: String
 
 
 func _ready() -> void:
-	yield(choose_game_path(), "completed")
+	game_path = (
+		yield(ask_game_dir(), "completed") if OS.has_feature("debug")
+		else OS.get_executable_path().get_base_dir()
+	)
+	
 	emit_signal("initialized")
-	print("Load main menu")
 	var err := get_tree().change_scene("res://scenes/mainmenu/mainmenu.tscn")
 	assert(err == OK)
 
 
-# Promp the user for the game path
-func choose_game_path() -> void:
-	print("Asking user for game path")
-	var filediag := FileDialog.new()
-	filediag.access = FileDialog.ACCESS_FILESYSTEM
-	filediag.mode = FileDialog.MODE_OPEN_DIR
-	filediag.window_title = "Choose the game directory"
+func ask_game_dir() -> String:
+	var dialog := FileDialog.new()
+	dialog.access = FileDialog.ACCESS_FILESYSTEM
+	dialog.mode = FileDialog.MODE_OPEN_DIR
+	dialog.window_title = "Select game directory"
+	add_child(dialog)
 
-	add_child(filediag)
-	filediag.popup_centered_minsize(Vector2(600, 500))
-	game_path = yield(filediag, "dir_selected")
-	filediag.queue_free()
+	dialog.popup_centered_minsize(Vector2(600, 500))
+	var path := yield(dialog, "dir_selected") as String
+	dialog.queue_free()
 
-	print("Game path: " + game_path)
+	return path
