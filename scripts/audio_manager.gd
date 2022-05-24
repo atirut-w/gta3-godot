ba06593c070b6f8e98feac46242b96d416148f4e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-05-25 00:30:33 +0700

    Remove some lines from Audio Manager

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -6,10 +6,7 @@ var _sfx_lookup := []
 
 func _ready() -> void:
 	yield(GameManager, "initialized")
-	print("Caching SFX lookup...")
-
 	_sfx_lookup = GTAAssetLoader.load_sfx_lookup(GameManager.game_path + "/audio/sfx.SDT")
-
 	print("Cached %d SFX entries" % _sfx_lookup.size())
 
 
